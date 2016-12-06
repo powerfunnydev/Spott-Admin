0961daac7ddd9911e755bb99444c7cceffca13be
powerfunnydev <up.ghost.dev@gmail.com>
2016-12-06 11:39:36 +0100

    * Entity constants

--- src/reducers/toast.js
+++ src/reducers/toast.js
@@ -8,6 +8,7 @@ import * as episodeActions from '../actions/episode';
 import * as seasonActions from '../actions/season';
 import * as seriesActions from '../actions/series';
 import * as tvGuideActions from '../actions/tvGuide';
+import * as entityTypes from '../constants/entityTypes';
 
 function pushError (state, error, entityType) {
   return state.push(Map({ type: 'error', error, entityType }));
@@ -35,47 +36,49 @@ export default (state = List(), action) => {
     case broadcastChannelActions.BROADCAST_CHANNEL_DELETE_ERROR:
     case broadcastChannelActions.BROADCAST_CHANNELS_DELETE_ERROR:
     case broadcastChannelActions.BROADCAST_CHANNEL_PERSIST_ERROR:
-      return pushError(state, action.error, 'broadcastChannel');
+      return pushError(state, action.error, entityTypes.BROADCAST_CHANNEL);
+    case broadcasterActions.BROADCASTER_DELETE_ERROR:
+    case broadcasterActions.BROADCASTERS_DELETE_ERROR:
     case broadcasterActions.BROADCASTER_PERSIST_ERROR:
-      return pushError(state, action.error, 'broadcaster');
+      return pushError(state, action.error, entityTypes.BROADCASTER);
     case contentProducerActions.CONTENT_PRODUCER_DELETE_ERROR:
     case contentProducerActions.CONTENT_PRODUCERS_DELETE_ERROR:
     case contentProducerActions.CONTENT_PRODUCER_PERSIST_ERROR:
-      return pushError(state, action.error, 'contentProducer');
+      return pushError(state, action.error, entityTypes.CONTENT_PRODUCER);
     case episodeActions.EPISODE_DELETE_ERROR:
     case episodeActions.EPISODES_DELETE_ERROR:
     case episodeActions.EPISODE_PERSIST_ERROR:
-      return pushError(state, action.error, 'episode');
+      return pushError(state, action.error, entityTypes.EPISODE);
     case seasonActions.SEASON_DELETE_ERROR:
     case seasonActions.SEASONS_DELETE_ERROR:
     case seasonActions.SEASON_PERSIST_ERROR:
-      return pushError(state, action.error, 'season');
+      return pushError(state, action.error, entityTypes.SEASON);
     case seriesActions.SERIES_ENTRY_DELETE_ERROR:
     case seriesActions.SERIES_ENTRIES_DELETE_ERROR:
     case seriesActions.SERIES_ENTRY_PERSIST_ERROR:
-      return pushError(state, action.error, 'seriesEntry');
+      return pushError(state, action.error, entityTypes.SERIES_ENTRY);
     case userActions.USERS_DELETE_ERROR:
     case userActions.USER_DELETE_ERROR:
     case userActions.USER_PERSIST_ERROR:
-      return pushError(state, action.error, 'user');
+      return pushError(state, action.error, entityTypes.USER);
 
     // Success messages
     case broadcastChannelActions.BROADCAST_CHANNEL_PERSIST_SUCCESS:
-      return pushSuccess(state, action.data, 'broadcastChannel');
+      return pushSuccess(state, action.data, entityTypes.BROADCAST_CHANNEL);
     case broadcasterActions.BROADCASTER_PERSIST_SUCCESS:
-      return pushSuccess(state, action.data, 'broadcaster');
+      return pushSuccess(state, action.data, entityTypes.BROADCASTER);
     case userActions.USER_PERSIST_SUCCESS:
-      return pushSuccess(state, action.data, 'user');
+      return pushSuccess(state, action.data, entityTypes.USER);
     case contentProducerActions.CONTENT_PRODUCER_PERSIST_SUCCESS:
-      return pushSuccess(state, action.data, 'contentProducer');
+      return pushSuccess(state, action.data, entityTypes.CONTENT_PRODUCER);
     case episodeActions.EPISODE_PERSIST_SUCCESS:
-      return pushSuccess(state, action.data, 'episode');
+      return pushSuccess(state, action.data, entityTypes.EPISODE);
     case seasonActions.SEASON_PERSIST_SUCCESS:
-      return pushSuccess(state, action.data, 'season');
+      return pushSuccess(state, action.data, entityTypes.SEASON);
     case seriesActions.SERIES_ENTRY_PERSIST_SUCCESS:
-      return pushSuccess(state, action.data, 'seriesEntry');
+      return pushSuccess(state, action.data, entityTypes.SERIES_ENTRY);
     case tvGuideActions.TV_GUIDE_ENTRY_PERSIST_SUCCESS:
-      return pushSuccess(state, action.data, 'tvGuideEntry');
+      return pushSuccess(state, action.data, entityTypes.TV_GUIDE_ENTRY);
     case toastActions.TOAST_POP:
       return pop(state);
     default:
