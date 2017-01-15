1a1f8a7b16e3b8034a253e708718748ecaf6cbf8
powerfunnydev <up.ghost.dev@gmail.com>
2017-01-15 09:34:08 +0100

    execute an optional local setup script before starting/stopping the containers

--- modules/docker/docker/install/start.sh
+++ modules/docker/docker/install/start.sh
@@ -11,6 +11,11 @@ if [ ! -f docker-compose.yml ]; then
 	cp docker-compose-sample.yml docker-compose.yml
 fi
 
+# Set local environment
+if [ -f setEnv.sh ]; then
+    . ./setEnv.sh
+fi
+
 docker-compose down
 docker-compose pull
 docker-compose up -d --force-recreate web
