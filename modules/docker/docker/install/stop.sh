524a874b585f66411e6ae386c2857977de86f714
powerfunnydev <up.ghost.dev@gmail.com>
2017-01-16 11:41:59 +0100

    stop script

---
+++ modules/docker/docker/install/stop.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Change to the script directory
+CWD=$(pwd)
+SCRIPT=$(readlink -f $0)
+SCRIPT_DIR=`dirname $SCRIPT`
+cd $SCRIPT_DIR
+
+# Start with sample
+if [ ! -f docker-compose.yml ]; then
+	cp docker-compose-sample.yml docker-compose.yml
+fi
+
+# Set local environment
+if [ ! -f setLocalEnv.sh ]; then
+    cp setLocalEnvSample.sh setLocalEnv.sh
+    chmod +x setLocalEnv.sh
+fi
+. ./setLocalEnv.sh
+
+# We need the deployment environment
+if [ -z "$DEPLOY_ENV" ]; then
+    echo "Error: No DEPLOY_ENV environment variable set. Please check the contents of setLocalEnv.sh"
+    exit 1
+fi
+
+# Stop
+docker-compose down
+
+cd $CWD
