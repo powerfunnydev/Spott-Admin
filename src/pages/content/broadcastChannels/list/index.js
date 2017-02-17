19a41a6bda970ee53954ce825cc497e9fab6f2bd
powerfunnydev <up.ghost.dev@gmail.com>
2017-02-18 05:23:30 +0800

    Apply listview to broadcastChannels.

--- src/pages/content/broadcastChannels/list/index.js
+++ src/pages/content/broadcastChannels/list/index.js
@@ -3,12 +3,12 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { Root, Container } from '../../../_common/styles';
-import { DropdownCel, isQueryChanged, Tile, tableDecorator, generalStyles, TotalEntries, headerStyles, NONE, sortDirections, CheckBoxCel, Table, Headers, CustomCel, Rows, Row, Pagination } from '../../../_common/components/table/index';
+import { isQueryChanged, Tile, tableDecorator, generalStyles, TotalEntries, Pagination } from '../../../_common/components/table/index';
 import Line from '../../../_common/components/line';
+import ListView from '../../../_common/components/listView/index';
 import Radium from 'radium';
 import * as actions from './actions';
 import selector from './selector';
-import Dropdown, { styles as dropdownStyles } from '../../../_common/components/actionDropdown';
 import { routerPushWithReturnTo } from '../../../../actions/global';
 import UtilsBar from '../../../_common/components/table/utilsBar';
 import { slowdown } from '../../../../utils';
@@ -17,8 +17,6 @@ import moment from 'moment';
 import { SideMenu } from '../../../app/sideMenu';
 import Header from '../../../app/multiFunctionalHeader';
 /* eslint-disable no-alert */
-
-const numberOfRows = 25;
 
 @tableDecorator()
 @connect(selector, (dispatch) => ({
@@ -81,6 +79,14 @@ export default class BroadcastChannelList extends Component {
     }
   }
 
+  determineReadUrl (broadcastChannel) {
+    return `/content/broadcast-channels/read/${broadcastChannel.get('id')}`;
+  }
+
+  determineEditUrl (broadcastChannel) {
+    return `/content/broadcast-channels/edit/${broadcastChannel.get('id')}`;
+  }
+
   getLastUpdatedOn (cp) {
     const date = new Date(cp.get('lastUpdatedOn'));
     return moment(date).format('YYYY-MM-DD HH:mm');
@@ -103,10 +109,17 @@ export default class BroadcastChannelList extends Component {
   }
 
   render () {
-    const { broadcastChannels, children, isSelected, location: { query, query: { display, page, searchString, sortField, sortDirection } },
+    const { broadcastChannels, children, deleteBroadcastChannel, isSelected, location: { query, query: { display, page, searchString, sortField, sortDirection } },
       pageCount, selectAllCheckboxes, selectCheckbox, totalResultCount,
       onChangeDisplay, onChangeSearchString } = this.props;
     const numberSelected = isSelected.reduce((total, selected, key) => selected && key !== 'ALL' ? total + 1 : total, 0);
+    const columns = [
+      { type: 'checkBox' },
+      { type: 'custom', sort: true, sortField: 'NAME', title: 'NAME', getUrl: this.determineReadUrl, name: 'name' },
+      { type: 'custom', title: 'UPDATED BY', name: 'lastUpdatedBy' },
+      { type: 'custom', sort: true, sortField: 'NAME', title: 'LAST UPDATED ON', name: 'lastUpdatedOn', dataType: 'date' },
+      { type: 'dropdown' }
+    ];
     return (
       <SideMenu>
         <Root>
@@ -133,35 +146,19 @@ export default class BroadcastChannelList extends Component {
               onDeleteSelected={this.onClickDeleteSelected}/>
             {(display === undefined || display === 'list') &&
               <div>
-                <Table>
-                  <Headers>
-                    {/* Be aware that width or flex of each headerCel and the related rowCel must be the same! */}
-                    <CheckBoxCel checked={isSelected.get('ALL')} name='header' style={[ headerStyles.header, headerStyles.firstHeader ]} onChange={selectAllCheckboxes}/>
-                    <CustomCel sortColumn={this.props.onSortField.bind(this, 'NAME')} sortDirection = {sortField === 'NAME' ? sortDirections[sortDirection] : NONE} style={[ headerStyles.header, headerStyles.notFirstHeader, headerStyles.clickableHeader, { flex: 2 } ]}>NAME</CustomCel>
-                    <CustomCel style={[ headerStyles.header, headerStyles.notFirstHeader, { flex: 2 } ]}>UPDATED BY</CustomCel>
-                    <CustomCel sortColumn={this.props.onSortField.bind(this, 'LAST_MODIFIED')} sortDirection = {sortField === 'LAST_MODIFIED' ? sortDirections[sortDirection] : NONE} style={[ headerStyles.header, headerStyles.notFirstHeader, headerStyles.clickableHeader, { flex: 2 } ]}>LAST UPDATED ON</CustomCel>
-                    <DropdownCel style={[ headerStyles.header, headerStyles.notFirstHeader ]}/>
-                  </Headers>
-                  <Rows isLoading={broadcastChannels.get('_status') !== 'loaded'}>
-                    {broadcastChannels.get('data').map((broadcastChannel, index) => {
-                      return (
-                        <Row index={index} isFirst={index % numberOfRows === 0} key={index} >
-                          {/* Be aware that width or flex of each headerCel and the related rowCel must be the same! */}
-                          <CheckBoxCel checked={isSelected.get(broadcastChannel.get('id'))} onChange={selectCheckbox.bind(this, broadcastChannel.get('id'))}/>
-                          <CustomCel style={{ flex: 2 }}>{broadcastChannel.get('name')}</CustomCel>
-                          <CustomCel style={{ flex: 2 }}>{broadcastChannel.get('lastUpdatedBy')}</CustomCel>
-                          <CustomCel getValue={this.getLastUpdatedOn} objectToRender={broadcastChannel} style={{ flex: 2 }}/>
-                          <DropdownCel>
-                            <Dropdown
-                              elementShown={<div key={0} style={[ dropdownStyles.clickable, dropdownStyles.option, dropdownStyles.borderLeft ]}onClick={() => { this.props.routerPushWithReturnTo(`/content/broadcast-channels/edit/${broadcastChannel.get('id')}`); }}>Edit</div>}>
-                              <div key={1} style={dropdownStyles.floatOption} onClick={async (e) => { e.preventDefault(); await this.deleteBroadcastChannel(broadcastChannel.get('id')); }}>Remove</div>
-                            </Dropdown>
-                          </DropdownCel>
-                        </Row>
-                      );
-                    })}
-                  </Rows>
-                </Table>
+                <ListView
+                  columns={columns}
+                  data={broadcastChannels}
+                  deleteItem={deleteBroadcastChannel}
+                  getEditUrl={this.determineEditUrl}
+                  isSelected={isSelected}
+                  load={() => this.props.load(this.props.location.query)}
+                  routerPushWithReturnTo={this.props.routerPushWithReturnTo}
+                  selectAllCheckboxes={selectAllCheckboxes}
+                  sortDirection={sortDirection}
+                  sortField={sortField}
+                  onCheckboxChange={(id) => selectCheckbox.bind(this, id)}
+                  onSortField={(name) => this.props.onSortField.bind(this, name)} />
                 <Pagination currentPage={(page && (parseInt(page, 10) + 1) || 1)} pageCount={pageCount} onLeftClick={() => { this.props.onChangePage(parseInt(page, 10), false); }} onRightClick={() => { this.props.onChangePage(parseInt(page, 10), true); }}/>
               </div>}
             {display === 'grid' &&
